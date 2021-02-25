579ac8faf416161a083a6c96f0e7218d36a94163
coseo12 <momo_12@naver.com>
2021-02-25 10:40:13 +0900

    project setup & firebase

---
+++ src/service/firebase.js
@@ -0,0 +1,11 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
