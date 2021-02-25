ce7fe9b914d0e6c61b5ef38ac775c63314a64dc6
coseo12 <momo_12@naver.com>
2021-02-25 17:18:07 +0900

    temp

---
+++ src/components/card-edit-form/card-edit-form.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './card-edit-form.module.css';
+
+const CardEditForm = ({ card }) => {
+  return <h1>{card.id}</h1>;
+};
+
+export default CardEditForm;
---
+++ src/components/card/card.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import styles from './card.module.css';
+
+const Card = ({ card }) => {
+  const {
+    name,
+    company,
+    title,
+    email,
+    message,
+    theme,
+    fileName,
+    fileURL,
+  } = card;
+  return <div></div>;
+};
+
+export default Card;
